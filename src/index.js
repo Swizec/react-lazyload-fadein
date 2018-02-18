d09fa65c0449faa515019ceaa402c6fc8766e561
Swizec Teller <swizec@swizec.com>
2018-02-17 22:32:50 -0800

    Initial setup using nwb

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+    render() {
+        return (
+            <div>
+                <h2>Welcome to React components</h2>
+            </div>
+        );
+    }
 }
