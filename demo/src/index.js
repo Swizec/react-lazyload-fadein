363aa491b8267d4983c3e01aaea2a9d76678039f
Swizec Teller <swizec@swizec.com>
2018-02-17 22:45:54 -0800

    Basic demo app

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,25 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import Example from '../../src'
+import FadeIn from "../../src";
 
 class Demo extends Component {
-  render() {
-    return <div>
-      <h1>react-lazyload-fadein Demo</h1>
-      <Example/>
-    </div>
-  }
+    render() {
+        return (
+            <div>
+                <h1>react-lazyload-fadein Demo</h1>
+                <FadeIn height={600}>
+                    {onload => (
+                        <img
+                            src="https://i.imgur.com/q067pyl.jpg"
+                            onLoad={onload}
+                            style={{ height: 600 }}
+                        />
+                    )}
+                </FadeIn>
+            </div>
+        );
+    }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-export * from "./FadeIn";
+import FadeIn from "./FadeIn";
+
+export default FadeIn;
+export { FadeIn };
