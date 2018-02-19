40938c6deb7ceafdae50dc6753af2403afd36af6
Swizec Teller <swizec@swizec.com>
2018-02-18 17:05:23 -0800

    Dataviz example

--- demo/src/Dataviz.js
+++ demo/src/Dataviz.js
@@ -1,43 +1,67 @@
 import React, { Component } from "react";
 import FadeIn from "../../src";
 import * as d3 from "d3";
+import chroma from "chroma-js";
 
 class PopulationChart extends Component {
     state = {
-        data: []
+        data: [],
+        error: null
     };
 
     componentDidMount() {
-        d3
-            .csv(
-                "./population.csv",
-                d => (
-                    console.log(d),
-                    {
-                        country: d.ISO,
-                        noc: d.NOC,
-                        name: d["Country name"],
-                        population: Number(d["pop.2010"].replace(/\./g, ""))
-                    }
-                )
-            )
-            .then(data => {
-                console.log(error, data);
+        d3.csv(
+            "https://raw.githubusercontent.com/Swizec/react-lazyload-fadein/master/demo/src/population.csv",
+            d => ({
+                country: d.ISO,
+                noc: d.NOC,
+                name: d["Country name"],
+                population: Number(d["pop.2010"].replace(/\./g, ""))
+            }),
+            (error, data) => {
                 this.setState({
-                    data
+                    data: data.filter(d => d.population > 0),
+                    error
                 });
                 this.props.onLoad();
-            });
+            }
+        );
     }
 
     render() {
-        const { data } = this.state;
+        const { error, data } = this.state;
 
-        return (
-            <React.Fragment>
-                <h2>hai</h2>
-            </React.Fragment>
-        );
+        const height = d3
+            .scaleLog()
+            .domain(d3.extent(data, d => d.population))
+            .range([0, 600]);
+        const width = d3
+            .scaleBand()
+            .domain(d3.range(data.length))
+            .range([0, 600]);
+        const color = chroma.scale(["yellow", "hotpink", "navy"]);
+
+        if (error) {
+            return <h2>Error loading data</h2>;
+        } else {
+            return (
+                <React.Fragment>
+                    <h2>Loaded {data.length} population datapoints</h2>
+                    <svg height="600" width="600">
+                        {data.map((d, i) => (
+                            <rect
+                                x={width(i)}
+                                y={600 - height(d.population)}
+                                height={height(d.population)}
+                                width={width.step()}
+                                key={d.country}
+                                fill={color(i / data.length)}
+                            />
+                        ))}
+                    </svg>
+                </React.Fragment>
+            );
+        }
     }
 }
 
